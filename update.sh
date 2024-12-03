4ecb1ac5d027ffae8a3e833445fc7247a5bef034
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2024-12-03 10:16:44 -0500

    Update to moonc v0.1.20241202+8756d160d
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- update.sh
+++ update.sh
@@ -2,4 +2,7 @@
 moon update && moon install && rm -rf target
 moon fmt
 moon test --target native
-moon test --target all
+# moon test --target all
+# 2024-12-03 - wasm target is now failing
+moon test --target wasm-gc
+moon test --target js
